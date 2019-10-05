722a21f5372ba23abd7d87813a9d1296030e960d
Edward Biswas <edbiswas@gmail.com>
2019-10-04 22:08:04 -0400

    Client

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import { Container } from "reactstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <h1>Hello</h1>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
